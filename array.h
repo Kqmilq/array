e8b7ed4a98bdc20c2648837636b006d8a98d35d6
Kamil <251010@edu.p.lodz.pl>
2024-05-17 07:11:26 +0200

    start

---
+++ array.h
@@ -0,0 +1,12 @@
+//
+// Created by Kamil on 17.05.2024.
+//
+
+#ifndef ARRAY_A_ARRAY_H
+#define ARRAY_A_ARRAY_H
+int FindMax(int tab[]);
+int FindMin(int tab[]);
+float FindAverage(int tab[]);
+void enterValue(int tab[]);
+void printArray(int tab[]);
+#endif //ARRAY_A_ARRAY_H
